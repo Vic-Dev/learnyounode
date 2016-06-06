8731b7f31ac4a7a3c53be79eac68c9656982f436
Victoria Johns <vmichellej@gmail.com>
2016-06-06 11:19:01 -0400

    Baby steps

--- program.js
+++ program.js
@@ -1,1 +1,9 @@
-console.log("HELLO WORLD");
+function sum(arr) {
+  var total = 0;
+  for (var i = 2; i < arr.length; i++) {
+    total += +arr[i];
+  }
+  return total;
+}
+
+console.log(sum(process.argv));
