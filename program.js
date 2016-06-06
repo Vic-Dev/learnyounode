63efa7afc4f9dcc9fc845bcc6d1a0d9cf17db26a
Victoria Johns <vmichellej@gmail.com>
2016-06-06 12:32:48 -0400

    Complete filtered ls challenge

--- program.js
+++ program.js
@@ -1,4 +1,12 @@
 var fs = require('fs');
+var p = require('path');
+
+var countLines;
+var listFiles = [];
+var path = process.argv[2];
+var fileExtension = process.argv[3];
+
+//  --- Challenge 2 ---
 
 function sum(arr) {
   var total = 0;
@@ -8,25 +16,45 @@ function sum(arr) {
   return total;
 }
 
+// --- Challenge 3 ---
+
 function newlines(path) {
   var buf = fs.readFileSync(path);
   var str = buf.toString();
   return (str.split(/\n/).length) - 1
 }
 
-var countLines;
-var path = process.argv[2];
+// --- Challenge 4 ---
 
 function newlines(callback) {
   fs.readFile(path, function doneReading(err, fileContents) {
     var str = fileContents.toString();
     countLines = ((str.split(/\n/).length) - 1);
     callback();
-  })
+  });
 }
 
 function returnCount() {
   console.log(countLines);
 }
 
-newlines(returnCount);
+// --- Challenge 5 ---
+
+function fileList(callback) {
+  fs.readdir(path, function doneReading(err, list) {
+    for (var i = 0; i < list.length; i++) {
+      if ( p.extname(list[i]) === "." + fileExtension ) {
+        listFiles.push(list[i]);
+      }
+    }
+    callback();
+  });
+}
+
+function printFiles() {
+  for (var i = 0; i < listFiles.length; i++) {
+    console.log(listFiles[i]);
+  }
+}
+
+fileList(printFiles);
